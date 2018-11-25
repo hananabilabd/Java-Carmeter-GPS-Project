52f2af8726bd45e6cac4794da4ff8df50f2122ce
BasmaEl-sayedIbrahim <basma.elsayed159@gmail.com>
2018-11-26 00:00:20 +0200

    updated gui

--- test/src/example/CarMeter.java
+++ test/src/example/CarMeter.java
@@ -14,11 +14,16 @@ import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 /**
@@ -27,12 +32,13 @@ import javafx.stage.Stage;
  */
 public class CarMeter extends Application implements Runnable {
     
-    @Override
+   @Override
     public void start(Stage primaryStage) {
 
         Button button1 = new Button("start");
         Button button2 = new Button("stop");
         Button button3 = new Button("Button 3");
+        
         
       button1.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -72,14 +78,36 @@ public class CarMeter extends Application implements Runnable {
                          .build();
         
         GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(25, 25, 25, 25));
 
         gridPane.add(button1, 0, 0, 1, 1);
         gridPane.add(button2, 1, 0, 1, 1);
         gridPane.add(button3, 2, 0, 1, 1);
+        gridPane.add(gauge, 3, 3, 2, 2);
         
-        gridPane.add(gauge, 3, 1, 2, 2);
-    
 
+   
+        Label lattitude = new Label("lattitude :");
+         lattitude.setFont(Font.font("Tahoma", FontWeight.THIN, 20));
+          gridPane.add(lattitude, 2, 10, 1, 1);
+          
+          TextField text = new TextField ();
+          gridPane.add(text, 3, 10, 1, 1);
+        
+         Label longitude = new Label("longitude :");
+         longitude.setFont(Font.font("Tahoma", FontWeight.THIN,20));
+         gridPane.add(longitude, 2, 15, 1, 1);
+          TextField text2 = new TextField ();
+          gridPane.add(text2, 3, 15, 1, 1);
+         
+         Label speed = new Label("speed :");
+         speed.setFont(Font.font("Tahoma", FontWeight.THIN, 20));
+         gridPane.add(speed, 2, 20, 1, 1);
+          TextField text3 = new TextField ();
+          gridPane.add(text3, 3, 20, 1, 1);
+        
         Scene scene = new Scene(gridPane, 240, 100);
         primaryStage.setTitle("CarMeter");
         primaryStage.setScene(scene);
