41188f142d4c1aeba1371f725184eaccea327039
BasmaEl-sayedIbrahim <basma.elsayed159@gmail.com>
2018-11-25 17:21:58 +0200

    put gps projrct in the repo

---
+++ GPS/src/gps/GPS.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gps;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author Basma
+ */
+public class GPS extends Application {
+    JFrame frame ;
+    
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        frame = new JFrame("FrameDemo");
+        
+        
+        
+        Button btn = new Button();
+        btn.setText("Get Map");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("get!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
