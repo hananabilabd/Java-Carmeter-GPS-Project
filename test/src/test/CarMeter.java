ea9fdb38cfdb7cf56ebd49015145d3334f7ebf1e
Hanna Nabil <hananabil@live.com>
2018-11-28 22:39:10 +0200

    added start and stop thread buttons functionality

--- test/src/test/CarMeter.java
+++ test/src/test/CarMeter.java
@@ -61,13 +61,16 @@ public class CarMeter extends Application implements MapComponentInitializedList
     TextField text_latitude;
     TextField text_longitude;
     TextField text_speed;
+    Thread thread_readLine;
+    int flag_position=0;
+    
     @Override
     public void init(){
         try {
             serialComm=new SerialCommunication();
             serialComm.connect();
-            Thread th = new Thread(new ReadLine());
-            th.start();
+            thread_readLine = new Thread(new ReadLine());
+            //thread_readLine.start();
          
         } catch (Exception ex) {
             System.out.println("Init Exception");
@@ -79,21 +82,34 @@ public class CarMeter extends Application implements MapComponentInitializedList
  
         Button button1 = new Button("start");
         Button button2 = new Button("stop");
+        button2.setDisable(true);
         Button button3 = new Button("Button 3");
         
-      button1.setOnAction(new EventHandler<ActionEvent>() {
+      button1.setOnAction(new EventHandler<ActionEvent>() {//start button
 
             @Override
             public void handle(ActionEvent event) {
-       
+                if (thread_readLine.isAlive()==false){
+            thread_readLine.start();}
+            else {
+                thread_readLine.resume();
+            }
+                button1.setDisable(true);button2.setDisable(false);
             }
         });
       
-       button2.setOnAction(new EventHandler<ActionEvent>() {
-
+       button2.setOnAction(new EventHandler<ActionEvent>() {//stop button
             @Override
             public void handle(ActionEvent event) {
-                
+                if (thread_readLine.isAlive()==true){
+                    thread_readLine.suspend();
+                }
+                button2.setDisable(true);
+                button1.setDisable(false);
+                text_latitude.clear();
+                text_longitude.clear();
+                text_speed.clear();
+                flag_position =0;
             }
         });
        
@@ -131,18 +147,21 @@ public class CarMeter extends Application implements MapComponentInitializedList
          label_lattitude.setFont(Font.font("Tahoma", FontWeight.THIN, 20));
           gridPane.add(label_lattitude, 2, 10, 1, 1);
           text_latitude = new TextField ();
+          text_latitude.setEditable(false);
           gridPane.add(text_latitude, 3, 10, 1, 1);
         
          Label label_longitude = new Label("longitude :");
          label_longitude.setFont(Font.font("Tahoma", FontWeight.THIN,20));
          gridPane.add(label_longitude, 2, 15, 1, 1);
           text_longitude = new TextField ();
+          text_longitude.setEditable(false);
           gridPane.add(text_longitude, 3, 15, 1, 1);
          
          Label speed = new Label("Speed :");
          speed.setFont(Font.font("Tahoma", FontWeight.THIN, 20));
          gridPane.add(speed, 2, 20, 1, 1);
           text_speed = new TextField ();
+          text_speed.setEditable(false);
           gridPane.add(text_speed, 3, 20, 1, 1);
           
           //gridPane.setGridLinesVisible(true);
@@ -192,16 +211,19 @@ public class CarMeter extends Application implements MapComponentInitializedList
                Thread.sleep(2000);
                //System.out.println("Calling showDirections from Java");
                Platform.runLater(() -> {
-                        i+=0.001;
+                   if (flag_position ==1){
+                        //i+=0.001;
                         map.removeMarker(marker);
                        markerOptions = new MarkerOptions();
-                       markerOptions.position( new LatLong(latitude+i,longitude+i) )
+                       markerOptions.position( new LatLong(latitude,longitude) )
                         .visible(Boolean.TRUE)
                         .title("My Marker");
                          marker = new Marker( markerOptions );
                         map.addMarker(marker);
-                        map.setCenter(new LatLong(latitude+i, longitude+i));
-                        
+                        map.setCenter(new LatLong(latitude, longitude));
+                   }
+                   else {map.clearMarkers();
+                   }
                        });
            } catch( Exception ex ) {
                ex.printStackTrace();
@@ -239,6 +261,7 @@ public class CarMeter extends Application implements MapComponentInitializedList
                             //System.out.println("latitude: " + gga.getPosition().getLatitude());
                             //System.out.println(",longitude: " + gga.getPosition().getLongitude());
                             System.out.println("GGA position: " + gga.getPosition());
+                            flag_position=1;
                     }
                         
                           }
